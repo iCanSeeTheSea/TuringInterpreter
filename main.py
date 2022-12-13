e26797b40ef1a6a7ffa8be765856433b45d1e2ee
sea <c.eres5519@gmail.com>
2022-12-13 09:07:01 +0000

    starting to implement FSM

--- main.py
+++ main.py
@@ -1,61 +1,63 @@
-class TransitionFunction:
-
-    def __init__(self):
-        self._mapping_table = {}
-
-    def transition(self, state, operand):
-        key = state + operand
-        return self._mapping_table[key]
-
-    def add_transition(self, key, output):
-        self._mapping_table[key] = output
-
-    def output(self):
-        print(self._mapping_table)
-
-
 class TuringMachine:
 
     def __init__(self):
-        self._transf = TransitionFunction()
+
+        self._mapping_table = {
+            'b': self._move_head('b', '>'),
+            'b+': self._assign_operator('o', '+', '>'),
+        }
         self._tape = input('').split(' ')
         print(self._tape)
 
         self._head = 0
         self._state = 'B'
-        self._memory = '0000'
+        self._operand = '0000'
+        self._operator = ''
+        self._cache = {'operator': '', 'operand': '', 'head': 0}
         self._step = True
 
-    def _execute(self):
-        (state, output, direction) = (c for c in self._transf.transition(
-            self._state, self._tape[self._head]))
+    def _move_head(self, state, direction):
         self._state = state
-        self._tape[self._head] = output
-        if direction == '<':
-            self._head -= 1
-            if self._head < 0:
-                self._tape = [' '] + self._tape
-                self._head = 0
-        if direction == '>':
-            self._head += 1
-            if self._head >= len(self._tape):
-                self._tape += ['  ']
+        match direction:
+            case '<':
+                self._head -= 1
+                if self._head < 0:
+                    self._head = 0
+            case '>':
+                self._head += 1
+                if self._head >= len(self._tape):
+                    self._tape += ['  ']
+
+    def _assign_operator(self, state, operator, direction):
+        self._state = state
+        self._operator = operator
+        if direction:
+            self._move_head(state, direction)
+
+    def _assign_operand(self, state, operand, direction):
+        self._state = state
+        self.operand = operand
+        if direction:
+            self._move_head(state, direction)
 
     def process(self):
         self.output_head()
         while self._state != 'T':
             if self._step:
                 _ = input('')
-            self._execute()
             self.output_head()
 
     def output_tape(self):
         print(
-            f"S{self._state}\n {'━┳━━'.join(['' for _ in range(len(self._tape) + 2)])}━┳━"
+            f"S{self._state}\n ━┳━{'━┳━'.join(['━━━━' for _ in range(len(self._tape))])}━┳━"
         )
-        print(f"{' ┃ '.join([' '] + self._tape + [' '])} ┃  ")
-        print(f" {'━┻━━'.join(['' for _ in range(len(self._tape) + 2)])}━┻━")
-        print(f"    {''.join(['    ' for _ in range(self._head)])}↑")
+        print(f"{' ┃ '.join([' '] + [c + ' '*(4-len(c)) for c in self._tape])} ┃  ")
+        print(
+            f" ━┻━{'━┻━'.join(['━━━━' for _ in range(len(self._tape))])}━┻━"
+        )
+        print(
+            f"    {''.join(['       ' for _ in range(self._head)])}↑"
+        )
 
     def output_head(self):
 
